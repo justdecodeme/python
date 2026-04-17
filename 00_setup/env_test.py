c8a373e433d307699ad8ae1cb814ae6e41d680e1
Rakesh Kumar <justdecodeme@hotmail.com>
2026-04-17 17:00:15 +0530

    env test doen

--- 00_setup/env_test.py
+++ 00_setup/env_test.py
@@ -1,5 +1,14 @@
+# --------------------- 
 # 🧪 Test your install
+# --------------------- 
+
+# 1. The Easter Egg (Open https://xkcd.com/353/)
 import antigravity
+
+# 2. The Zen of Python (Reveals the 19 guiding principles for writing "good" Python code (The Zen of Python, by Tim Peters))
 import this
 
+# 3. The Classic
 print("Environment is ready! 🚀")
+print("Hello, Rakesh! 🚀")
+print("Welcome to Python! 🐍")
--- 00_setup/hello.py
+++
@@ -1,7 +0,0 @@
-print("Hello, Rakesh! 🚀")
-print("Welcome to Python! 🐍")
-
-# import antigravity
-
-# Reveals the 19 guiding principles for writing "good" Python code (The Zen of Python, by Tim Peters)
-# import this

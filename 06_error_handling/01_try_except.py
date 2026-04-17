14fa51ac2a03966faaf665b677f7b97565933121
Rakesh Kumar <justdecodeme@hotmail.com>
2026-04-18 00:00:05 +0530

    06_error_handling

--- 06_error_handling/01_try_except.py
+++ 06_error_handling/01_try_except.py
@@ -1,9 +1,29 @@
 # 🚨 Try/Except: Error catching
 # Don't let your program crash! 🚑
 
+print("\n--- Try/Except ---")
 try:
     result = 10 / 0
 except ZeroDivisionError:
     print("Oops! You can't divide by zero! 💥")
 finally:
     print("This runs no matter what! 🏁")
+
+print("\n--- Try/Except/Else ---")
+try:
+    num = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid input! Please enter a number. ❌")
+else:
+    print(f"You entered: {num} ✅")
+
+
+print("\n--- Try/Except/Else/Finally ---")
+try:
+    num = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid input! Please enter a number. ❌")
+else:
+    print(f"You entered: {num} ✅")
+finally:
+    print("Done! 🏁")

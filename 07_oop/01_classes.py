400a899753d1925a1902ea5def923b6aae1397c8
Rakesh Kumar <justdecodeme@hotmail.com>
2026-04-18 00:20:05 +0530

    07_oop

--- 07_oop/01_classes.py
+++ 07_oop/01_classes.py
@@ -1,12 +1,16 @@
 # 🏗️ Classes: Object-Oriented Programming
 # Blueprints for creating objects 🏠
 
+print("\n--- Classes ---")
+
+
 class Dog:
     def __init__(self, name):
         self.name = name
-    
+
     def bark(self):
         print(f"{self.name} says Woof! 🐶")
+
 
 my_dog = Dog("Buddy")
 my_dog.bark()
--- 07_oop/02_inheritance.py
+++ 07_oop/02_inheritance.py
@@ -1,14 +1,33 @@
 # 🧬 Inheritance: Sharing code between classes
 # Passing down traits! 👨‍👦
 
+print("\n--- Inheritance ---")
+
+
 class Animal:
     def eat(self):
         print("Munch munch munch! 🍲")
+
 
 class Cat(Animal):
     def meow(self):
         print("Meow! 🐱")
 
+
+class Dog(Animal):
+    def bark(self):
+        print("Woof! 🐶")
+
+    def eat(self):
+        print("Bone! 🦴")
+
+
+print("\nCat:")
 my_cat = Cat()
-my_cat.eat() # Inherited from Animal
+my_cat.eat()  # Inherited from Animal
 my_cat.meow()
+
+print("\nDog:")
+my_dog = Dog()
+my_dog.eat()  # Overridden from Animal
+my_dog.bark()

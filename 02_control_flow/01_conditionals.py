84b7a8764fb8ccae460f7889ca475ddaffe8059c
Rakesh Kumar <justdecodeme@hotmail.com>
2026-04-17 22:42:17 +0530

    formatting

--- 02_control_flow/01_conditionals.py
+++ 02_control_flow/01_conditionals.py
@@ -1,7 +1,10 @@
 # 🔀 Conditionals: if / elif / else
 # Let's make some decisions! 🤔
 
+print("...........")
+
 weather = "sunny"
+
 if weather == "sunny":
     print("Wear sunglasses! 🕶️")
 elif weather == "rainy":

21df45c536a47595b6c11d487c33f1a8ce1f01cf
Rakesh Kumar <justdecodeme@hotmail.com>
2026-04-18 14:32:24 +0530

    list

--- 03_data_structures/01_list.py
+++ 03_data_structures/01_list.py
@@ -6,3 +6,11 @@ inventory = ["sword ⚔️", "shield 🛡️", "potion 🧪"]
 inventory.append("map 🗺️")
 print(f"Inventory: {inventory}")
 print(type(inventory))  # <class 'list'>
+
+# List: containing TUPLES
+# Each tuple acts as a fixed "record"
+staff = [("Ram", 16), ("Zara", 17), ("Raj", 15)]
+
+print("...........")
+print(type(staff))  # <class 'list'>
+print(type(staff[0]))  # <class 'tuple'>

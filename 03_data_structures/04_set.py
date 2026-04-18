8b41b8f59b70ae741a3371a11c14136a94a45a7a
Rakesh Kumar <justdecodeme@hotmail.com>
2026-04-18 11:31:09 +0530

    set

--- 03_data_structures/04_set.py
+++ 03_data_structures/04_set.py
@@ -6,3 +6,19 @@ unique_colors = {"red 🔴", "green 🟢", "blue 🔵"}
 unique_colors.add("red 🔴")  # Won't be added again!
 print(f"Unique colors: {unique_colors}")
 print(type(unique_colors))  # <class 'set'>
+
+print("...........")
+# This is the constructor method. It is the most versatile way to create a set.
+my_siblings = set(["s1", "s2", "s3"])
+print(f"My siblings: {my_siblings}")
+print(type(my_siblings))  # <class 'set'>
+
+print("...........")
+my_list = [1, 2, 2, 3]
+unique_set = set(my_list)
+print(f"Unique set: {unique_set}")  # Unique set: {1, 2, 3}
+print(type(unique_set))  # <class 'set'>
+
+print("...........")
+not_a_set = {}
+print(type(not_a_set))  # <class 'dict'> ⬅️ Watch out for this!
